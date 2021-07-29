63bf2ce8ee16576eedc059c3c1d872584bf76795
BaekDoHyung <qorehgud@cnrres.com>
2021-07-29 15:16:12 +0900

    기본 hooks의 router 사용 example

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <>
+            <h2>About</h2>
+            <div>이동 한 About 메뉴 입니다.</div>
+        </>
+    );
+}
+
+export default About;
