334a7b04b29e814b82eeaf591e33410d1940986d
BaekDorazi <qorehgud@gmail.com>
2021-11-14 22:23:57 +0900

    react-image-gallery 라이브러리 추가하여 이미지 슬라이드 컴포넌트 추가

--- src/view/main/MainContent.jsx
+++ src/view/main/MainContent.jsx
@@ -1,6 +1,32 @@
 import React, {useCallback} from 'react';
+import ImageGallery from 'react-image-gallery';
+
+import 'react-slideshow-image/dist/styles.css';
+import sample1 from "../../assets/sample1.jpg";
+import sample2 from "../../assets/sample2.jpg";
+import sample3 from "../../assets/sample3.jpg";
+import sample4 from "../../assets/sample4.jpg";
 
 const MainContent = () => {
+    const images = [
+        {
+            original: sample1,
+            thumbnail: sample1
+        },
+        {
+            original: sample2,
+            thumbnail: sample2
+        },
+        {
+            original: sample3,
+            thumbnail: sample3
+        },
+        {
+            original: sample4,
+            thumbnail: sample4
+        }
+    ];
+
     const getProductComp = useCallback(() => {
         const sampleData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
@@ -19,6 +45,16 @@ const MainContent = () => {
 
     return (
         <div className="main-content-container">
+            <ImageGallery
+                items={images}
+                onSlide={true}
+                infinite={true}
+                showBullets={true}
+                showPlayButton={false}
+                showFullscreenButton={false}
+                showThumbnails={false}
+                slideDuration={1000}/>
+
             <div className="grid-container">
                 {getProductComp()}
             </div>
